242f280a26d4c251e201f0ac989a9a2593acbc4d
RorryThePop <johs07@mail.ru>
2022-05-08 23:42:41 +0300

    fix burger

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-const name = document.getElementById('name')
-const email = document.getElementById('mail')
-const telephone = document.getElementById('phone')
+let burgerLine = document.querySelector('.burger');
+
+let nav = document.querySelector('.nav')
+
+let count = 0
+
+burgerLine.addEventListener('click', e => {
+    count += 1;
+    if (count % 2 == 0) {
+        nav.style.transform = 'scale(1,0)'
+    } else {
+        nav.style.transform = 'scale(1,1)'
+    }
+})
